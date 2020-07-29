c6aa02cf404ba7cea3520dc58fa6b0565b706ff9
WellingtonFidelis <wellingtonf20@gmail.com>
2020-07-28 22:33:38 -0300

    criação das pages, slider

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
-        <img className="Logo" srcSet={Logo} alt="WellFlix logo" />
-      </a>
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
-        New movie
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
       </Button>
     </nav>
   );
 }
+
 export default Menu;
